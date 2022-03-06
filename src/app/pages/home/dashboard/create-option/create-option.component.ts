e1623bdac5441438c57314c029a329cc69139a46
Gerald Tagle <78480062+GcTags@users.noreply.github.com>
2022-03-07 00:53:54 +0800

    Create create-option.component.ts

---
+++ src/app/pages/home/dashboard/create-option/create-option.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateDialogComponent } from '../create-dialog/create-dialog.component';
+
+@Component({
+  selector: 'app-create-option',
+  templateUrl: './create-option.component.html',
+  styleUrls: ['./create-option.component.scss']
+})
+export class CreateOptionComponent implements OnInit {
+
+  constructor(private matDialog: MatDialog,
+    private _bottomSheetRef: MatBottomSheetRef) { }
+
+  ngOnInit(): void {
+  }
+  openCreateDialog(createMsg: string){
+    this._bottomSheetRef.dismiss();
+
+    const dialogConfig = this.matDialog.open(CreateDialogComponent, {
+      data:{
+        type: createMsg
+      },
+      autoFocus: true,
+      disableClose: false
+    });
+
+    dialogConfig.afterClosed().subscribe((res: any) => {
+        if (res) {
+          console.log(res)
+          // this.dataSource.data.push(res)
+          // this.table.renderRows();
+          }
+    });
+
+  }
+
+}
