4a0ccdd2aaf1fbce9b83f2f8711e9152daf46431
Gerald Tagle <78480062+GcTags@users.noreply.github.com>
2022-06-03 22:22:32 +0800

    Update addstudents.component.ts

--- src/app/pages/presentation/rightnav/addstudents/addstudents.component.ts
+++ src/app/pages/presentation/rightnav/addstudents/addstudents.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import { MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { DataService } from 'src/app/services/data.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -12,29 +16,110 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 
+export interface Students {
+  name: string;
+  emailadd: string;
+}
+
+
 @Component({
   selector: 'app-addstudents',
   templateUrl: './addstudents.component.html',
   styleUrls: ['./addstudents.component.scss']
 })
 export class AddstudentsComponent implements OnInit {
+    step1Complete =  false;
+    step2Complete = false;
+    firstFormGroup!: FormGroup;
+    secondFormGroup!: FormGroup;
+
+    classid: number;
+    studentsid: any = 0
+    students: any = []
+    classes: any = []
     studentlists: any = []
-    fetchedstuds: any = []
+
     selected: any;
     inputValue: string= '';
     ImageLink = this._us.imageLink;
     emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
+    isAdd: number
 
-  constructor(private _ds: DataService, private _us: UserService,    private _snackBar: MatSnackBar,
+    displayedColumns: string[] = ['#','class','program','name', 'emailadd', 'action'];
+    dataSource = new MatTableDataSource<Students>();
 
-    ) { }
+  constructor(  
+    @Inject (MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<AddstudentsComponent>,
+    private _formBuilder: FormBuilder,
+    private _ds: DataService, 
+    private _us: UserService,    
+    private _snackBar: MatSnackBar,
+
+    ) {
+      if(data){
+       this.isAdd = data.status
+      }
+
+     }
+
+     @ViewChild(MatSort) sort!: MatSort;
+     @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit(): void {
-  this.showStudentInstantly()
-  this.getStudentslist()
+    if(this.isAdd){
+      this.showClasses()
+      this.classesFormgroup()
+
+    }else{
+      this.getStudentslist()
+     
+    }
   }
 
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  classesFormgroup(){
+    this.firstFormGroup = this._formBuilder.group({
+      class_id: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      student_id: ['', Validators.required],
+
+    });
+  }
+
+  showClasses() {
+
+    this._ds.processData1('assignquiz/showClasses/instructor', this._us.getUserID(), 2)?.subscribe((res: any)=>{
+      let load = this._ds.decrypt(res.d)
+      this.classes = load
+    }, err =>{
+    
+        console.log('Error',err)
+     
+    });
+  
+  }
+
+  selectClass(classid:any){
+    this.classid = classid;
+    this._ds.processData1('assignquiz/getStudentsinClass/instructor', classid, 2)?.subscribe((res: any)=>{
+      let load = this._ds.decrypt(res.d)
+      this.students = load
+      console.log(load)
+    }, err =>{
+    
+        console.log('Error',err)
+     
+    });
+  }
 
   showStudentInstantly(){
 
@@ -53,9 +138,9 @@ export class AddstudentsComponent implements OnInit {
   getStudentslist(){
     this._ds.processData1('assignquiz/getallassigned/students', this._us.getPresentationId(), 2)?.subscribe((res: any)=>{
       let load = this._ds.decrypt(res.d)
-
-      this.fetchedstuds = load
       console.log(load)
+      this.dataSource = new MatTableDataSource(load); 
+
     }, err =>{
     
         console.log('Error',err)
@@ -64,17 +149,16 @@ export class AddstudentsComponent implements OnInit {
   }
 
   addStudents(){
-    if(this.selected){
-      this._ds.processData1('assignquiz/add/students',{ 
-        quizid_fld:  this._us.getPresentationId(),
-        accountno_fld: this.selected,
-      }, 2)?.subscribe((res: any)=>{
-        this.selected = ''
-        this.getStudentslist()
+    if(this.firstFormGroup.valid  && this.secondFormGroup.valid){
+      
+      let studentsData = this.countStudents();
+      
+      this._ds.processData1('assignquiz/add/students',studentsData, 2)?.subscribe((res: any)=>{
+
+        this.dialogRef.close(true);
 
         this._snackBar.open('',"New student has been added", {
           duration: 2000,
-          panelClass: ['login-snackbar']
         });
   
       }, err =>{
@@ -85,7 +169,30 @@ export class AddstudentsComponent implements OnInit {
     }
   
   }
-  // 0414A
+
+  countStudents(){
+    var data=[];
+
+    if(this.studentsid.length){
+      for(let i = 0; this.studentsid.length > i; i++){
+  
+
+        if(i <= this.studentsid.length){
+          data.push({
+            quizid_fld: this._us.getPresentationId(),
+            classid_fld: this.firstFormGroup.get('class_id')?.value,
+            accountno_fld:this.studentsid[i]
+          });
+        
+        }
+      }
+    }
+    return JSON.stringify(data)
+  }
+
+  change(studentid: any) {
+    this.studentsid = studentid
+   }
 
   clearInput(e:any){
     console.log(e)
@@ -99,11 +206,10 @@ export class AddstudentsComponent implements OnInit {
 
   removeStudent(id: number, accountno: number, index: number){
     this._ds.processData1(`assignquiz/delete/students/${accountno}`,id, 2)?.subscribe((res: any)=>{
-      this.fetchedstuds.splice(index, 1);
-
+      this.dataSource.data.splice(index, 1);
+      this.getStudentslist()
       this._snackBar.open('',"Student has been removed", {
         duration: 2000,
-        panelClass: ['login-snackbar']
       });
 
     }, err =>{
