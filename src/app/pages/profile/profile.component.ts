b91c633d355e771f33efcd0b00e22fc3d359a9e7
Gerald Tagle <78480062+devTags@users.noreply.github.com>
2022-06-26 00:34:51 +0800

    Update profile component

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { map, shareReplay } from 'rxjs/operators';
 
 import { Observable } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-profile',
@@ -19,6 +20,9 @@ export class ProfileComponent implements OnInit {
   version: string = this._user.appVersion
   profilepic: string = this._user.getProfileImage();
 
+  presCount: number = 0;
+  quizCount: number = 0;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
   .pipe(
     map(result => result.matches),
@@ -26,9 +30,33 @@ export class ProfileComponent implements OnInit {
   );
 
 
-  constructor( private breakpointObserver: BreakpointObserver, private _user: UserService) { }
+  constructor( private breakpointObserver: BreakpointObserver, private _user: UserService, private _ds: DataService) { }
 
   ngOnInit(): void {
+    this.getInstructorTotalQuiz()
+    this.getInstructorTotalPres()
+  }
+
+  getInstructorTotalQuiz(){
+    this._ds.processData1(`slides/byUserId/${1}`, this._user.getUserID(), 2)?.subscribe((res: any) => {
+      let load = this._ds.decrypt(res.d);
+      this.quizCount = load.length
+      // console.log(this.quizCount)
+
+      },err =>{
+        // console.log('err', err)
+      });
+  }
+
+  getInstructorTotalPres(){
+    this._ds.processData1(`slides/byUserId/${0}`, this._user.getUserID(), 2)?.subscribe((res: any) => {
+      let load = this._ds.decrypt(res.d);
+      this.presCount = load.length
+
+      
+      },err =>{
+        // console.log('err', err)
+      });
   }
 
 }
