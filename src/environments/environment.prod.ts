576d8bb55810ff7a5d0a681eaa1bf2d43f4b40f9
Gerald Tagle <78480062+devTags@users.noreply.github.com>
2022-06-26 00:34:55 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  webLink: `https://gordoncollegeccs.edu.ph/projects/gcclip/dist/instructor/#/presentation/`,
+  webLink: `https://gordoncollegeccs.edu.ph/projects/gcclip/apps/instructor/#/presentation/`,
   apiLink: `https://gordoncollegeccs.edu.ph:4230/${btoa('api').replace('=', '')}/`,
   imageLink: `https://gordoncollegeccs.edu.ph:4230/${btoa('api').replace('=', '')}/${btoa('uploads').replace('=', '')}/`,
   socket: `https://gordoncollegeccs.edu.ph:4231`,
